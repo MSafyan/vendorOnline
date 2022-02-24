46e7b233f142bd4f1f220e02479a9ff1ca6d0be9
Salman Wasi <salmanwasi8@gmail.com>
2022-02-24 23:20:51 +0500

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+import Router from './routes';
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/routes/index.js
@@ -0,0 +1,25 @@
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import BarLoader from 'react-bar-loader';
+
+const Home = lazy(() => import('../screens/Home'));
+const Profile = lazy(() => import('../screens/Profile'));
+const PostJob = lazy(() => import('../screens/PostJob'));
+const Job = lazy(() => import('../screens/Job'));
+const Jobs = lazy(() => import('../screens/Jobs'));
+
+const Router = () => {
+  return (
+    <Suspense fallback={<BarLoader height={5} width={100} color="#00b754" />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/post-job" element={<PostJob />} />
+        <Route path="/jobs/:id" element={<Job />} />
+        <Route path="/jobs" element={<Jobs />} />
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default Router;
