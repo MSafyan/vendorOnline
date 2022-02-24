bfff490e490288b05a5b6e8efc2a24fc27d821e8
Salman Wasi <salmanwasi8@gmail.com>
2022-02-24 23:03:39 +0500

    Basic Structure

---
+++ server/app.js
@@ -0,0 +1,48 @@
+//***Importing Modules
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const socketIo = require('socket.io');
+const apiRoutes = require('./routes');
+// const deserializeUser = require('./src/middlewares/deserializeUser');
+const setEnv = require('./utils/setEnv');
+
+setEnv();
+
+// App Configuration
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
+
+// socket
+// const io = socketIo(server, {
+//   cors: {
+//     origin: '*',
+//   },
+//   pingTimeout: 4000,
+//   pingInterval: 12000,
+// });
+
+// global.io = io;
+// require('./src/socketManger/socketManger')(io);
+
+//Middlwares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
+// app.use(deserializeUser);
+
+app.use(express.static('build'));
+app.use('/public', express.static('public'));
+// API Routes
+app.use('/api/', apiRoutes);
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./build", "index.html"));
+// });
+
+server.listen(PORT, () => {
+  console.log(`Server Is running on ${process.env.BASE_URL}`);
+});
